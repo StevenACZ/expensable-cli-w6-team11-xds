109dd655375948ce50e464a74dd461e62caef9e8
stevenacz <scoailazaa@gmail.com>
2020-11-26 23:15:23 -0500

    method add_transaction created

--- transaction.rb
+++ transaction.rb
@@ -4,4 +4,10 @@ module Transaction
   def load_transactions(category_id)
     @transactions = TransactionController.index(@user[:token], category_id)
   end
+
+  def add_transaction(category_id)
+    new_transaction_data = transaction_form
+
+    @transactions.push(TransactionController.add(@user[:token], category_id, new_transaction_data))
+  end
 end
